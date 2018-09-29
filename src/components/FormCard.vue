bff848294dff87881cdb889592344fcaae7e7490
Quentin MICHINOT <qmichinot@wynd.eu>
2018-09-29 22:03:55 +0200

    feat(formCard): add style on element

--- src/components/FormCard.vue
+++ src/components/FormCard.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="add_car">
-    <label for="itemFrom">Lien d'image</label>
-    <input type="text" id="itemFormSrc" v-on:keypress.enter="addItem">
+    <h2 class="title_add_car">Ajouter un Avatar</h2>
+    <input type="text" id="itemFormSrc" v-on:keypress.enter="addItem" placeholder="Lien de l'image">
     <br>
-    <label for="itemFormName">Nom</label>
-    <input type="text" id="itemFormName" v-on:keypress.enter="addItem">
+    <input type="text" id="itemFormName" v-on:keypress.enter="addItem" placeholder="Nom">
     <br>
     <button v-on:click="addItem">Ajouter</button>
   </div>
@@ -16,12 +15,22 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 .add_car {
   position: absolute;
   right: 0;
-  background-color: white;
+  background-color: rgba(0, 0, 0, 0.5);
   padding: 50px;
   top: 0;
+
+  input {
+    width: 100%;
+    margin: 0 0 15px 0;
+    padding: 5px;
+  }
+  .title_add_car {
+    margin: 0 0 15px 0;
+    color: white;
+  }
 }
 </style>
